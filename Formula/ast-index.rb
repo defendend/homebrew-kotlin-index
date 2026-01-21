75ab2d492cff2e1a56e74d161d4a39d9ffebff5b
defendend <ivsacha@mail.ru>
2026-01-21 17:44:48 +0300

    Update to v3.8.1

--- Formula/ast-index.rb
+++ Formula/ast-index.rb
@@ -1,16 +1,16 @@
 class AstIndex < Formula
   desc "Fast code search CLI for Android/Kotlin/Java, iOS/Swift/ObjC, Perl, Python, Go, Proto, WSDL, and C++ projects"
   homepage "https://github.com/defendend/Claude-ast-index-search"
-  url "https://github.com/defendend/Claude-ast-index-search/releases/download/v3.8.0/ast-index-3.8.0-darwin-arm64.tar.gz"
-  sha256 "f22b1e89d45d3312217b2333192cb4e16f38507331c244ab62ade7cefe3386e2"
+  url "https://github.com/defendend/Claude-ast-index-search/releases/download/v3.8.1/ast-index-3.8.1-darwin-arm64.tar.gz"
+  sha256 "e0611887e0426b43dbe8ecb1deb76d86e1a6b37b678e206773fa3be2b9cc5216"
   license "MIT"
-  version "3.8.0"
+  version "3.8.1"
 
   def install
     bin.install "ast-index"
   end
 
   test do
-    assert_match "ast-index v3.8.0", shell_output("#{bin}/ast-index version")
+    assert_match "ast-index v3.8.1", shell_output("#{bin}/ast-index version")
   end
 end
